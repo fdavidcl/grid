da57eb81645fbb8abc025f23f99812394be456ba
David Charte <fdavidcl@protonmail.com>
2018-04-24 21:02:29 +0200

    First upload

---
+++ js/main.js
@@ -0,0 +1,32 @@
+var app = new Vue({
+  el: "#app",
+  data: {
+    showModal: false,
+    groups: [
+      { title: "Personal", links: [
+        { title: "Profile", href: "http://david.quotient.space"},
+        { title: "Mail", href: "https://mail.protonmail.com"},
+        { title: "Github", href: "https://github.com"},
+        { title: "Lychee", href: "https://photos.quotient.space"},
+        { title: "Profile", href: "http://david.quotient.space"},
+        { title: "Mail", href: "https://mail.protonmail.com"},
+        { title: "Github", href: "https://github.com"},
+        { title: "Lychee", href: "https://photos.quotient.space"},
+        { title: "Profile", href: "http://david.quotient.space"},
+        { title: "Mail", href: "https://mail.protonmail.com"},
+        { title: "Github", href: "https://github.com"},
+        { title: "Lychee", href: "https://photos.quotient.space"}
+      ]},
+      { title: "Social", links: [
+        { title: "Mastodon", href: "https://mstdn.io"},
+        { title: "Nosykitty", href: "http://github.com/fdavidcl/ask/pulls "}
+      ]},
+      { title: "Música", links: [
+        { title: "study music", href: "https://open.spotify.com/user/fdavidcl/playlist/4Uny81Vv83plUNBBwb30Fr"},
+        { title: "Hacker's Coffee", href: "https://open.spotify.com/user/omegak/playlist/4mWcjE2mVcaALYi6v8hDZN"},
+        { title: "suomeksi", href: "https://open.spotify.com/user/fdavidcl/playlist/2J77wfgIPNST4Ql5zzk7L9" }
+        
+      ]}
+    ]
+  }
+})
